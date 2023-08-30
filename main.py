72d9e16677fbb13eb2afc39fbafc130a10d684c1
root <root@localhost>
2023-08-30 02:43:59 +0200

    1.2.0

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import bot
+import json
 
 if __name__ == '__main__':
+    # define env - should be an argument with default value later on
+    env='live'
 
-  bot.run_bot()
+    # read meta + config
+    meta=json.loads(open('meta.json').read())
+    config=json.loads(open('config.json').read())[env]
+
+    # run
+    bot.run_bot(meta,config)
